61458355914a8071fb930c88feef53c94368bd74
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-04 11:11:32 -0300

    ✨ fix: implementation new items component expense item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 export function ExpenseItem() {
   return (
-    <h2>Expense item</h2>
+    <div>
+      <div>MArch 28th 2021</div>
+
+      <div>
+        <h2>Car Insurance</h2>
+
+        <div>$ 294.67</div>
+      </div>
+    </div>
   )
 }
