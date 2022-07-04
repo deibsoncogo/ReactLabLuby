27b94d851c97be4ea5e5dc6fdbee27fa12ae8d0d
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-04 12:45:33 -0300

    ✨ feat expenseDate: implementing component

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,15 @@
+import './ExpenseDate.css'
+
+export function ExpenseDate(props) {
+  const month = props.date.toLocaleString('pt-br', { month: 'long' });
+  const day = props.date.toLocaleString('pt-br', { month: '2-digit' });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className='expense-date'>
+      <div className='expense-date-month'>{month}</div>
+      <div className='expense-date-year'>{year}</div>
+      <div className='expense-date-day'>{day}</div>
+    </div>
+  )
+}
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,10 @@
+import { ExpenseDate } from './ExpenseDate'
 import './ExpenseItem.css'
 
 export function ExpenseItem(props) {
-  const month = props.date.toLocaleString('pt-br', { month: 'long'});
-  const day = props.date.toLocaleString('pt-br', { month: '2-digit'});
-  const year = props.date.getFullYear();
-
   return (
     <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
 
       <div className='expense-item-description'>
         <h2>{props.title}</h2>
