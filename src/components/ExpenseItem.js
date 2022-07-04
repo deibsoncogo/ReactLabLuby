b24ab9f34000bcd5519466f5a85f47a85e45e2aa
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-04 11:41:32 -0300

    ✨ fix: implementing javascript html

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,18 @@
 import './ExpenseItem.css'
 
 export function ExpenseItem() {
+  const expenseDate = new Date()
+  const expenseTitle = 'Car Insurance'
+  const expenseAmount = 294.67
+
   return (
     <div className='expense-item'>
-      <div>March 28th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
 
       <div className='expense-item-description'>
-        <h2>Car Insurance</h2>
+        <h2>{expenseTitle}</h2>
 
-        <div className='expense-item-price'>$ 294.67</div>
+        <div className='expense-item-price'>$ {expenseAmount}</div>
       </div>
     </div>
   )
