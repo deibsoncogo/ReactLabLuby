5f2b7c4b29a1a05ec93a5d3c8f13a78daa20fea9
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-04 11:55:09 -0300

    ✨ fix: implementing property html component expense item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,14 @@
 import './ExpenseItem.css'
 
-export function ExpenseItem() {
-  const expenseDate = new Date()
-  const expenseTitle = 'Car Insurance'
-  const expenseAmount = 294.67
-
+export function ExpenseItem(props) {
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
 
       <div className='expense-item-description'>
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
 
-        <div className='expense-item-price'>$ {expenseAmount}</div>
+        <div className='expense-item-price'>$ {props.amount}</div>
       </div>
     </div>
   )
