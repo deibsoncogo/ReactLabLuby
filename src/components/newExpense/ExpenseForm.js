4db4a8aa312e1bd0c31cc7d3669101da3b5b2f96
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-05 14:10:02 -0300

    ✨ feat expenseForm: implementing concept immutability for to save values input

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -1,27 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseForm.css'
 
 export const ExpenseFrom = () => {
-  const titleChangeHandle = (event) => {
-    console.log(event.target.value)
-  }
+  const [enteredTitle, setEnteredTitle] = useState('')
+  const [enteredAmount, setEnteredAmount] = useState('')
+  const [enteredDate, setEnteredDate] = useState('')
+
+  const titleChangeHandle = (event) => { setEnteredTitle(event.target.value) }
+  const amountChangeHandle = (event) => { setEnteredAmount(event.target.value) }
+  const dateChangeHandle = (event) => { setEnteredDate(event.target.value) }
 
   return (
     <form>
       <div className='new-expense-controls'>
         <div className='new-expense-control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandle}/>
+          <input type='text' onChange={titleChangeHandle} />
         </div>
 
         <div className='new-expense-control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' />
+          <input type='number' min='0.01' step='0.01' onChange={amountChangeHandle} />
         </div>
 
         <div className='new-expense-control'>
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' />
+          <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandle} />
         </div>
       </div>
 
