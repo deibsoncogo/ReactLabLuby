19362427198268f6d5fa9bb47792732e1c825442
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-05 14:49:04 -0300

    ⚗️ feat expenseForm: implemeting form submit

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -10,22 +10,38 @@ export const ExpenseFrom = () => {
   const amountChangeHandle = (event) => { setEnteredAmount(event.target.value) }
   const dateChangeHandle = (event) => { setEnteredDate(event.target.value) }
 
+  const submitHandler = (event) => {
+    event.preventDefault()
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    }
+
+    console.log(expenseData)
+
+    setEnteredTitle('')
+    setEnteredAmount('')
+    setEnteredDate('')
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense-controls'>
         <div className='new-expense-control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandle} />
+          <input type='text' onChange={titleChangeHandle} value={enteredTitle} />
         </div>
 
         <div className='new-expense-control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' onChange={amountChangeHandle} />
+          <input type='number' min='0.01' step='0.01' onChange={amountChangeHandle} value={enteredAmount} />
         </div>
 
         <div className='new-expense-control'>
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandle} />
+          <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandle} value={enteredDate} />
         </div>
       </div>
 
