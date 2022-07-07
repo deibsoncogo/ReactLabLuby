6f884b428d35e25e246261833e1f2a9a6defabe1
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-07 15:21:39 -0300

    ♻️ fix: creation message zero expense

--- src/components/expense/Expenses.js
+++ src/components/expense/Expenses.js
@@ -20,14 +20,18 @@ export const Expenses = (props) => {
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
 
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {filteredExpenses.length === 0 ? (
+          <p>No expenses found</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )}
       </Card>
     </div>
   )
