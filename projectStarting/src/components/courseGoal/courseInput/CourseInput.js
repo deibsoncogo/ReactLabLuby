7a4fb660388f7f310c73b6b0ade7ad7e98610067
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-08 11:20:19 -0300

    💄 feat: implemention style error

--- projectStarting/src/components/courseGoal/courseInput/CourseInput.js
+++ projectStarting/src/components/courseGoal/courseInput/CourseInput.js
@@ -4,21 +4,33 @@ import './CourseInput.css'
 
 export const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('')
+  const [isValid, setIsValid] = useState(true)
 
   const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim() > 0) { setIsValid(true) }
     setEnteredValue(event.target.value)
   }
 
   const formSubmitHandler = (event) => {
     event.preventDefault()
+
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false)
+      return
+    }
+
     props.onAddGoal(enteredValue)
   }
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
-        <label>Course goal</label>
-        <input type='text' onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? 'red' : 'black' }}>Course goal</label>
+        <input
+          style={{ borderColor: !isValid ? 'red' : '#ccc', background: !isValid ? 'salmon' : 'transparent' }}
+          type='text'
+          onChange={goalInputChangeHandler}
+        />
       </div>
 
       <Button type='submit'>Add goal</Button>
