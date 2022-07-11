6dfa5420f71585f7cb55bf39e76c1151028fbeda
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-11 10:10:12 -0300

    🚧 feat: implemention class 95

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { AddUser } from './components/users/addUser'
+import { UserList } from './components/users/userList'
 
 export function App() {
   return (
-    <div><AddUser /></div>
+    <div>
+      <AddUser />
+      <UserList users={[]} />
+    </div>
   )
 }
