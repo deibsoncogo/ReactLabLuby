5633b99ee40e283aa310f9f81ac1f4d7f64f789f
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-11 09:32:53 -0300

    🚧 feat: implemention class 91

--- project3/src/components/users/addUser.js
+++ project3/src/components/users/addUser.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Card } from '../ui/card'
+import style from './addUser.module.css'
 
 export const AddUser = (props) => {
   const addUserHandler = (event) => {
@@ -6,14 +8,16 @@ export const AddUser = (props) => {
   }
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor='userName'>User name</label>
-      <input type='text' id='userName'/>
+    <Card className={style.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor='userName'>User name</label>
+        <input type='text' id='userName' />
 
-      <label htmlFor='age'>Age (Years)</label>
-      <input type='number' id='age' />
+        <label htmlFor='age'>Age (Years)</label>
+        <input type='number' id='age' />
 
-      <button type='submit'>Add user</button>
-    </form>
+        <button type='submit'>Add user</button>
+      </form>
+    </Card>
   )
 }
