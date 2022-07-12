9765a2b11edf54eac99f43544d40afac590c2d03
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-12 15:14:15 -0300

    🚧 feat: implemention class 123

--- project4/src/components/navigation/index.js
+++ project4/src/components/navigation/index.js
@@ -1,33 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { AuthContext } from '../../store/authContext'
 import style from './style.module.css'
 
 export const Navigation = (props) => {
+  const ctx = useContext(AuthContext)
+
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav className={style.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href='/'>Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href='/'>Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        )
-      }}
-    </AuthContext.Consumer>
+    <nav className={style.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href='/'>Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href='/'>Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   )
 }
