b4397bc93c9ffae7df858fa0033435e73c04493b
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-12 14:57:55 -0300

    🚧 feat: implemention class 121

--- project4/src/components/home/index.js
+++ project4/src/components/home/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from "../ui/button";
 import { Card } from '../ui/card'
 import style from './style.module.css'
 
@@ -6,6 +7,7 @@ export const Home = (props) => {
   return (
     <Card className={style.home}>
       <h1>Welcome back</h1>
+      <Button onCLick={props.onLogout}>Logout</Button>
     </Card>
   )
 }
