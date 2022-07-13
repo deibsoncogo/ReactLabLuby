e07290ac74afef33e2f0582f1071bd62a29a5cad
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-13 18:41:00 -0300

    🚧 feat project5: implemention class 139

---
+++ project5/src/components/cart/cart/index.js
@@ -0,0 +1,27 @@
+import style from './style.module.css'
+
+export const Cart = (props) => {
+  const cartItems = (
+    <ul className={style['cart-items']}>
+      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  )
+
+  return (
+    <div>
+      {cartItems}
+
+      <div className={style.total}>
+        <span>Total amount</span>
+        <span>35.62</span>
+      </div>
+
+      <div className={style.actions}>
+        <button className={style['button-alt']}>Close</button>
+        <button className={style.button}>Order</button>
+      </div>
+    </div>
+  )
+}
