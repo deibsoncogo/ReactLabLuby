aa67acef2779f97341309dcc1a3bde5de4da2c0e
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-14 10:34:04 -0300

    🚧 feat project5: implemention class 144

--- project5/src/store/cartProvider.js
+++ project5/src/store/cartProvider.js
@@ -1,16 +1,35 @@
-import { CartContext } from "./cartContext"
+import { useReducer } from 'react'
+import { CartContext } from './cartContext'
+
+const defaultCartState = { items: [], totalAmount: 0 }
+const cartReducer = (state, action) => {
+  if (action.type === 'add') {
+    const updatedItems = state.items.concat(action.item)
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+
+    return { items: updatedItems, totalAmount: updatedTotalAmount }
+  }
+
+  return defaultCartState
+}
 
 export const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {}
-  const removeItemToCartHandler = (item) => {}
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: 'add', item })
+  }
+
+  const removeItemToCartHandler = (item) => {
+    dispatchCartAction({ type: 'remove', item })
+  }
 
   const CartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemToCartHandler,
   }
-
 
   return (
     <CartContext.Provider value={CartContext}>
