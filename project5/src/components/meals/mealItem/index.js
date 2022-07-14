b63e0009da3b962571abb1a29ddbfd2d07bee159
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-14 11:05:15 -0300

    🚧 feat project5: implemention class 145

--- project5/src/components/meals/mealItem/index.js
+++ project5/src/components/meals/mealItem/index.js
@@ -1,8 +1,16 @@
+import { useContext } from 'react'
+import { CartContext } from '../../../store/cartContext'
 import { MealItemForm } from '../mealItemForm'
 import style from './style.module.css'
 
 export const MealItem = (props) => {
+  const cartCtx = useContext(CartContext)
+
   const price = `$${props.price.toFixed(2)}`
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({ id: props.id, name: props.name, amount, price: props.price })
+  }
 
   return (
     <li className={style.meal}>
@@ -13,7 +21,7 @@ export const MealItem = (props) => {
       </div>
 
       <div>
-        <MealItemForm />
+        <MealItemForm onAddToCart={addToCartHandler} />
       </div>
     </li>
   )
--- project5/src/components/ui/input/index.js
+++ project5/src/components/ui/input/index.js
@@ -1,10 +1,11 @@
+import React from 'react'
 import style from './style.module.css'
 
-export const Input = (props) => {
+export const Input = React.forwardRef((props, ref) => {
   return (
     <div className={style.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   )
-}
+})
