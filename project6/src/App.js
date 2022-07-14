6014f15fb8bbfe2a847de564530205a2db8b42df
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-14 17:15:55 -0300

    🚧 feat project6: implemention class 153

--- project6/src/App.js
+++ project6/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button } from './components/ui/button'
 import './app.css'
 
 export function App() {
+  const [showParagraph, setShowParagraph] = useState(false)
+
+  console.log('APP RUNNING')
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph)
+  }
+
   return (
     <div className='app'>
       <h1>Hi there</h1>
+
+      {showParagraph && <p>This is new</p>}
+
+      <Button onClick={toggleParagraphHandler}>Toggle paragraph</Button>
     </div>
   )
 }
