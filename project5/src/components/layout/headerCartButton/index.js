c40bf8aa0c77bb17555ac9f3ff094125eded570f
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-14 10:16:59 -0300

    🚧 feat project5: implemention class 143

--- project5/src/components/layout/headerCartButton/index.js
+++ project5/src/components/layout/headerCartButton/index.js
@@ -1,12 +1,20 @@
+import { useContext } from 'react'
+import { CartContext } from '../../../store/cartContext'
 import { CartIcon } from '../../cart/cartIcon'
 import style from './style.module.css'
 
 export const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext)
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount
+  }, 0)
+
   return (
     <button className={style.button} onClick={props.onClick}>
       <span className={style.icon}><CartIcon /></span>
       <span>Your Cart</span>
-      <span className={style.badge}>3</span>
+      <span className={style.badge}>{numberOfCartItems}</span>
     </button>
   )
 }
