f9ae76833fa703926ba4c5022a63d2dd85ec630d
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-15 14:34:50 -0300

    🚧 feat project7: class implementation 166

--- project7/src/components/users/index.js
+++ project7/src/components/users/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { Component, useState } from 'react'
 import { User } from '../user'
 import style from './style.module.css'
 
@@ -8,28 +8,65 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ]
 
-export const Users = () => {
-  const [showUsers, setShowUsers] = useState(true)
-
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState)
+export class Users extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showUsers: true,
+      more: 'Test'
+    }
   }
 
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  )
+  toggleUsersHandler() {
+    // this.state.showUsers = false
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers }
+    })
+  }
 
-  return (
-    <div className={style.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
-      </button>
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    )
 
-      {showUsers && usersList}
-    </div>
-  )
+    return (
+      <div className={style.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+
+        {this.state.showUsers && usersList}
+      </div>
+    )
+  }
 }
+
+// export const Users = () => {
+//   const [showUsers, setShowUsers] = useState(true)
+
+//   const toggleUsersHandler = () => {
+//     setShowUsers((curState) => !curState)
+//   }
+
+//   const usersList = (
+//     <ul>
+//       {DUMMY_USERS.map((user) => (
+//         <User key={user.id} name={user.name} />
+//       ))}
+//     </ul>
+//   )
+
+//   return (
+//     <div className={style.users}>
+//       <button onClick={toggleUsersHandler}>
+//         {showUsers ? 'Hide' : 'Show'} Users
+//       </button>
+
+//       {showUsers && usersList}
+//     </div>
+//   )
+// }
