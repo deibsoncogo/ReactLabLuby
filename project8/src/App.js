87c437f5c2cbed13610945af6114127c408ce25a
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-18 10:52:49 -0300

    🚧 feat project7: class implementation 179

--- project8/src/App.js
+++ project8/src/App.js
@@ -4,8 +4,11 @@ import './app.css'
 
 export function App() {
   const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   async function fetchMoviesHandler() {
+    setIsLoading(true)
+
     const response = await fetch('https://swapi.dev/api/films/')
     const data = await response.json()
 
@@ -19,6 +22,7 @@ export function App() {
     })
 
     setMovies(transformedMovies)
+    setIsLoading(false)
   }
 
   return (
@@ -28,7 +32,10 @@ export function App() {
       </section>
 
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length ===  0 && <p>Found no movies</p>}
+
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   )
