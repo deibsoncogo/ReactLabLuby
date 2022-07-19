72bb1baf5f50c2e2d284a4c43252779e75730a8f
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-19 09:56:22 -0300

    🚧 feat project10: implementation class 202 (Providing Validation Feedback)

--- project10/src/components/simpleInput/index.js
+++ project10/src/components/simpleInput/index.js
@@ -3,6 +3,7 @@ import { useRef, useState } from "react"
 export const SimpleInput = (props) => {
   const nameInputRef = useRef()
   const [enteredName, setEnteredName] = useState('')
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true)
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value)
@@ -12,8 +13,11 @@ export const SimpleInput = (props) => {
     event.preventDefault()
 
     if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false)
       return
     }
+
+    setEnteredNameIsValid(true)
 
     console.log('enteredName =>', enteredName)
 
@@ -23,9 +27,11 @@ export const SimpleInput = (props) => {
     setEnteredName('')
   }
 
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid'
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input
           ref={nameInputRef}
@@ -35,6 +41,8 @@ export const SimpleInput = (props) => {
           value={enteredName}
         />
       </div>
+
+      {!enteredNameIsValid && <p className='error-text'>name must not be empty</p>}
 
       <div className='form-actions'>
         <button>Submit</button>
