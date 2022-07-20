809a2e5bb406ada7550c7ebf431c9a5e01f64ba2
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-20 12:03:49 -0300

    🚧 feat project11: implementation class 220 (Reading Form Values)

--- project11/src/components/cart/checkout/index.js
+++ project11/src/components/cart/checkout/index.js
@@ -1,30 +1,41 @@
+import { useRef } from 'react'
 import style from './style.module.css'
 
 export const Checkout = (props) => {
+  const nameInputRef = useRef()
+  const streetInputRef = useRef()
+  const postalInputRef = useRef()
+  const cityInputRef = useRef()
+
   const confirmHandler = (event) => {
     event.preventDefault()
+
+    const enteredName = nameInputRef.current.value
+    const enteredStreet = streetInputRef.current.value
+    const enteredPostalCode = postalCodeInputRef.current.value
+    const enteredCity = cityInputRef.current.value
   }
 
   return (
     <form className={style.form} onSubmit={confirmHandler}>
       <div className={style.control}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' ref={nameInputRef} id='name' />
       </div>
 
       <div className={style.control}>
         <label htmlFor='street'>Street</label>
-        <input type='text' id='street' />
+        <input type='text' ref={streetInputRef} id='street' />
       </div>
 
       <div className={style.control}>
         <label htmlFor='postal'>Postal Code</label>
-        <input type='text' id='postal' />
+        <input type='text' ref={postalInputRef} id='postal' />
       </div>
 
       <div className={style.control}>
         <label htmlFor='city'>City</label>
-        <input type='text' id='city' />
+        <input type='text' ref={cityInputRef} id='city' />
       </div>
 
       <div className={style.actions}>
