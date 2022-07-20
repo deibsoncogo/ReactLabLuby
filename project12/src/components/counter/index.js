8975bc7e93e747ec16daceddfc74e961d4d4329e
Deibson Henrique Cogo <deibson.henrique@luby.software>
2022-07-20 17:12:45 -0300

    🚧 feat project12: implementation class 236 (Dispatching Actions From Inside Components)

--- project12/src/components/counter/index.js
+++ project12/src/components/counter/index.js
@@ -1,16 +1,25 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import style from './style.module.css'
 
 export const Counter = () => {
+  const dispatch = useDispatch()
   const counter = useSelector((state) => { return state.counter })
 
   const toggleCounterHandler = () => { }
+
+  const incrementHandler = () => { return dispatch({ type: 'increment' }) }
+  const decrementHandler = () => { return dispatch({ type: 'decrement' }) }
 
   return (
     <main className={style.counter}>
       <h1>Redux Counter</h1>
 
       <div className={style.value}>{counter}</div>
+
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
 
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
