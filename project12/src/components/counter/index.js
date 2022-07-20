e4b99ef7e08c306d54f6f05822c7fc0fdd5e4ced
Deibson Henrique Cogo <deibson.henrique@luby.software>
2022-07-20 15:54:34 -0300

    🚧 feat project12: implementation class 235 (Using Redux Data in React Components)

--- project12/src/components/counter/index.js
+++ project12/src/components/counter/index.js
@@ -1,13 +1,16 @@
+import { useSelector } from 'react-redux'
 import style from './style.module.css'
 
 export const Counter = () => {
+  const counter = useSelector((state) => { return state.counter })
+
   const toggleCounterHandler = () => { }
 
   return (
     <main className={style.counter}>
       <h1>Redux Counter</h1>
 
-      <div className={style.value}>-- COUNTER VALUE --</div>
+      <div className={style.value}>{counter}</div>
 
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
