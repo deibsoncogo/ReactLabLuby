3fd5d7240f86fe819fb8370789c516564dce69e8
Deibson Henrique Cogo <deibson.henrique@luby.software>
2022-07-21 14:28:02 -0300

    🚧 feat project13: implementation class 253 (Refresher / Practice: Part 2/2)

--- project13/src/store/cart-slice.js
+++ project13/src/store/cart-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-createSlice({
+const cartSlice = createSlice({
   name: 'cart',
   initialState: { items: [], totalQuantity: 0 },
   reducers: {
@@ -8,9 +8,11 @@ createSlice({
       const newItem = action.payload
       const existingItem = state.items.find(item => item.id === newItem.id)
 
+      state.totalQuantity++
+
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -20,6 +22,23 @@ createSlice({
         existingItem.quantity++
         existingItem.totalPrice = existingItem.totalPrice + newItem.price
       }
+    },
+
+    removeItemFromCart(state, action) {
+      const id = action.payload
+      const existingItem = state.items.find(item => item.id === id)
+
+      state.totalQuantity--
+
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id)
+      } else {
+        existingItem.quantity--
+      }
     }
   }
 })
+
+export const cartActions = cartSlice.actions
+
+export default cartSlice
